f26a2b97042d5eb55cc1aaa1df33e63f134b39f0
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-05-20 11:52:09 -0300

    Update exercise_04_safe_average.py

--- exercise_04_safe_average.py
+++ exercise_04_safe_average.py
@@ -28,4 +28,16 @@ def safe_average(filename):
         # archivo contiene: "10\n20\nno_es_un_numero\n30\n"
         safe_average("numeros.txt") -> 20.0
     """
-    pass  # Reemplazar con tu implementación
+    suma = 0
+    cantidad = 0
+    with open(filename, 'r') as archivo:
+        for linea in archivo:
+            try:
+                numero = float(linea.strip())
+                suma += numero
+                cantidad +=1
+            except ValueError:
+                pass
+    if cantidad ==0:
+        raise ValueError("no valid numbers")
+    return suma / cantidad
