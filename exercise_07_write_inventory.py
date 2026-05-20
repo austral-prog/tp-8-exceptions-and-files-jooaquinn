3a77801a447599649303bcb870606eb865948b4f
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-05-20 11:53:27 -0300

    Update exercise_07_write_inventory.py

--- exercise_07_write_inventory.py
+++ exercise_07_write_inventory.py
@@ -28,4 +28,6 @@ def write_inventory(filename, inventory):
         # iron:7
         # wood:10
     """
-    pass  # Reemplazar con tu implementación
+     with open(filename, 'w') as archivo:
+        for clave in sorted(inventory):
+            archivo.write(f'{clave}:{inventory[clave]}\n')
